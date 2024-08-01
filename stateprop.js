56313e8af38e28245c97b1f413b9c3e3db36be11
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-08-01 20:36:48 +1000

    Update stateprop.js

--- stateprop.js
+++ stateprop.js
@@ -1,5 +1,5 @@
 window.onload = (event) => { 
     console.log("StateProp.js: Set State Prop")
      //// prop 0 enables YT player, 1 for screen cast
-    setPublicSpaceProp('firestateprop', '1');
+    setPublicSpaceProp('firestateprop', '0');
   };
