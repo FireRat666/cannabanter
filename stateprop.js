7ee86e16417a4e1f10d07e39896872dd0a968a49
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-08-04 12:00:04 +1000

    Update stateprop.js

--- stateprop.js
+++ stateprop.js
@@ -1,5 +1,11 @@
-window.onload = (event) => { 
-    console.log("StateProp.js: Set State Prop")
-     //// prop 0 enables YT player, 1 for screen cast
-    setPublicSpaceProp('firestateprop', '1');
-  };
+// window.onload = (event) => { 
+//     console.log("StateProp.js: Set State Prop")
+//      //// prop 0 enables YT player, 1 for screen cast
+//     setPublicSpaceProp('firestateprop', '1');
+//   };
+
+const cannascene = BS.BanterScene.getInstance();
+cannascene.On("loaded", () => {
+	console.log("STATEPROP: scene loaded, setting prop value");
+    setPublicSpaceProp('firestateprop', '0');
+	})
