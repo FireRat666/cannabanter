bad635e93122e5a6ec9666ee5d08cefc6433ecce
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-08-30 15:07:46 +1000

    Update stateprop.js

--- stateprop.js
+++ stateprop.js
@@ -96,7 +96,7 @@ function enableScreenStuff2() {
 	firescreen.setAttribute("backdrop", "false");
 	firescreen.setAttribute("hand-controls", "true");
 	firescreen.setAttribute("volume", "0.25");
-	firescreen.setAttribute("website", "https://www.youtube.com/embed/YDfiTGGPYCk?si=FN5DAq1va3bnKpdM");
+	firescreen.setAttribute("website", "https://www.youtube.com/embed/jfKfPfyJRdk?autoplay=1&controls=0");
 	firescreen.setAttribute("src", "https://firer.at/scripts/firescreen.js");
 	document.querySelector("a-scene").appendChild(firescreen);
   }
