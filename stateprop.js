b9bb562c445d7d6614539366de36b9d21be2cf28
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-11-06 11:21:17 +1100

    Update stateprop.js

--- stateprop.js
+++ stateprop.js
@@ -62,7 +62,7 @@ function enableScreenStuff2() {
 	firescreen.id = "cannabanter-firescreen";
 	firescreen.setAttribute("scale", "0.515 0.515 1");
 	firescreen.setAttribute("rotation", "0 0 0");
-	firescreen.setAttribute("position", "0 1.5 -8");
+	firescreen.setAttribute("position", "0 0 -8");
 	firescreen.setAttribute("mipmaps", "1");
 	firescreen.setAttribute("pixelsperunit", "1600");
 	// firescreen.setAttribute("castmode", "true");
@@ -74,7 +74,7 @@ function enableScreenStuff2() {
 	firescreen.setAttribute("volume", "0.25");
 	firescreen.setAttribute("width", "1920");
 	firescreen.setAttribute("height", "1080");
-	firescreen.setAttribute("screen-position", "0 -4.5 -0.02");
+	firescreen.setAttribute("screen-position", "0 -3 0");
 	firescreen.setAttribute("website", websiteurl);
 	firescreen.setAttribute("src", "https://firer.at/scripts/firescreenv2.js");
 	document.querySelector("a-scene").appendChild(firescreen);
