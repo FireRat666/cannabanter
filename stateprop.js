e433b6b9d22a65ed9537b703cb418b21fa6e6eb1
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-11-06 11:26:00 +1100

    Update stateprop.js

--- stateprop.js
+++ stateprop.js
@@ -74,7 +74,7 @@ function enableScreenStuff2() {
 	firescreen.setAttribute("volume", "0.25");
 	firescreen.setAttribute("width", "1920");
 	firescreen.setAttribute("height", "1080");
-	firescreen.setAttribute("screen-position", "0 -5.5 0");
+	firescreen.setAttribute("screen-position", "0 -8 0");
 	firescreen.setAttribute("website", websiteurl);
 	firescreen.setAttribute("src", "https://firer.at/scripts/firescreenv2.js");
 	document.querySelector("a-scene").appendChild(firescreen);
