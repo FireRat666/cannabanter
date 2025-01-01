1817def019bb7bdc5bee41c1c5c744ec660651c1
FireRat <27763388+FireRat666@users.noreply.github.com>
2025-01-01 16:10:08 +1100

    Update bullshcript.js

--- bullshcript.js
+++ bullshcript.js
@@ -11,7 +11,7 @@ CannaTest PLzgofGqYK8u6AlSuAxhkaYzgkWBvDupWR
 /* YouTube Live - https://www.youtube.com/embed/***********?autoplay=1&controls=0 */ /* Use this for line 14. replace *********** after embed the url for Drac live session */
 
 /* CHANGE THIS URL FOR SCREENCAST OR YOUTUBE LIVE */
-let websiteurl = "https://cannabanter.firer.at/embed.html"; /* ?autoplay=1&controls=0 For YouTube Live */
+let websiteurl = "https://cannabanter.firer.at/embed.html?21"; /* ?autoplay=1&controls=0 For YouTube Live */
 
 let otherwebsiteurl = "https://firer.at/pages/games.html"; // Small Screen Up Stairs
 
