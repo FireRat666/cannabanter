9293403d43f3bb4e5637d37c20b05e3e9be32727
Vanquish3r <153280214+vanquish3r@users.noreply.github.com>
2025-01-06 22:38:20 -0700

    Update bullshcript.js

--- bullshcript.js
+++ bullshcript.js
@@ -9,9 +9,10 @@ CannaTest PLzgofGqYK8u6AlSuAxhkaYzgkWBvDupWR
 
 /* Screencast - https://screen.sdq.st:8443/?room=cannabanter */ /* This one for gamestream. remove publish. put in line 14 */
 /* YouTube Live - https://www.youtube.com/embed/***********?autoplay=1&controls=0 */ /* Use this for line 14. replace *********** after embed the url for Drac live session */
+/* Fire Embed - https://cannabanter.firer.at/embed.html?21 */
 
 /* CHANGE THIS URL FOR SCREENCAST OR YOUTUBE LIVE */
-let websiteurl = "https://cannabanter.firer.at/embed.html?21"; /* ?autoplay=1&controls=0 For YouTube Live */
+let websiteurl = "https://screen.sdq.st:8443/?room=cannabanter"; /* ?autoplay=1&controls=0 For YouTube Live */
 
 let otherwebsiteurl = "https://firer.at/pages/games.html"; // Small Screen Up Stairs
 
