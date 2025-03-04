6b84fd02b04b6aaf13faa9c4f59b07f6ea795c26
Vanquish3r <153280214+vanquish3r@users.noreply.github.com>
2025-03-04 02:59:16 -0700

    Update bullshcript.js

--- bullshcript.js
+++ bullshcript.js
@@ -77,7 +77,7 @@ let ytplayerdisabled = true;
 	  	videoplayer.setAttribute("spatial", "false");
 		// videoplayer.setAttribute("spatial-min-distance", "1");
 		// videoplayer.setAttribute("spatial-max-distance", "500");
-		videoplayer.setAttribute("playlist", "PLZWiw-xxQ4SNl-dmJhSk6xSnGuALdCZGf");
+		videoplayer.setAttribute("playlist", "PLZWiw-xxQ4SPDmADhvme7-pU2bx3s7nKX");
 		videoplayer.setAttribute("announce", "false");
 		// videoplayer.setAttribute("announce-four-twenty", "false");
 		videoplayer.setAttribute("data-playlist-icon-url", "https://cdn.glitch.global/47f0acb4-4420-4f3f-bb01-dba17f8c0edb/Playlist.png?v=1711786451727");
