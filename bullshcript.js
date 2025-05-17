1189adb8f738add5eff6ebb343b6763f12200a86
Vanquish3r <153280214+vanquish3r@users.noreply.github.com>
2025-05-16 19:02:06 -0600

    Update bullshcript.js

--- bullshcript.js
+++ bullshcript.js
@@ -150,7 +150,7 @@ let karaokeplayerdisabled = true;
 		videoplayer.setAttribute("position", "0 -3 8");
 		videoplayer.setAttribute("hand-controls", "true");
 		videoplayer.setAttribute("button-position", "-2.815 6.5775 15.3");
-		videoplayer.setAttribute("volume", "10");
+		videoplayer.setAttribute("volume", "15");
 		videoplayer.setAttribute("button-rotation", "0 90 0");
 		videoplayer.setAttribute("button-scale", "0.7 0.7 0.7");
 		videoplayer.setAttribute("singer-button-position", "0 -50 0");
